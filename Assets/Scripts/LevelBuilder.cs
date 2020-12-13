316feff6ddccb9137fde78df24057fb09d347cf6
Jason Eastman <jasont41@gmail.com>
2020-12-13 05:16:54 -0800

    Got some randomization working

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -6,23 +6,65 @@ public class LevelBuilder : MonoBehaviour
 {
 
     public GameObject GeneralTilePrefab;
+    public GameObject BelowTop;
+    private float spacingMultiple = 0.18f;
+    private int currentTile; 
+    private int firstRandomSeed = 3;
+    public int[] heightArray; 
+    
 
-    public float CurrentTileLength; // must be even number 
+    
+    
+    
     private float incrementingVariable; 
     // Start is called before the first frame update
     void Start()
     {
-        incrementingVariable = CurrentTileLength / 5; 
-        //Instantiate(GeneralTilePrefab, new Vector3 (0, 0, 0), Quaternion.identity); 
-        for(float i = 0f; i < CurrentTileLength; i += 0.18f)
+        currentTile = 0; 
+        for(int currVal = 0; currVal < 5; currVal++) // Always flat for the first five tiles 
         {
-            Instantiate(GeneralTilePrefab, new Vector3(i, 0, 0), Quaternion.identity);
+            float temp = currVal * spacingMultiple;
+            currentTile++; 
+            Instantiate(GeneralTilePrefab, new Vector3(temp, 0, 0), Quaternion.identity);
         }
+        CalculatingHeights(); 
+
+    }
+    private void CalculatingHeights()
+    {
+         
+        if (currentTile <= heightArray.Length) { 
+            Chances();
+            Debug.Log(1);
+        }   
+    }
+    private void Chances()
+    {
+        int tempRand = Random.Range(1, 4); 
+        if(tempRand == firstRandomSeed)
+        {
+            heightArray[currentTile] = 1;
+            float tempY = heightArray[currentTile] * spacingMultiple;
+            float tempX = currentTile * spacingMultiple;
+            Instantiate(GeneralTilePrefab, new Vector3(tempX, tempY, 0), Quaternion.identity);
+            fillBottomTiles(heightArray[currentTile],tempX); 
+            Debug.Log(2); 
+        }
+        else
+        {
+            float tempX = currentTile * spacingMultiple;
+            Instantiate(GeneralTilePrefab, new Vector3(tempX, 0, 0), Quaternion.identity);
+            Debug.Log(3); 
+        }
+        currentTile++;
+        CalculatingHeights(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void fillBottomTiles(int val, float posX)
     {
-        
+        for(int i = 0; i < val; i++)
+        {
+            Instantiate(BelowTop, new Vector3(posX, (spacingMultiple * i),0), Quaternion.identity);
+        }
     }
 }
